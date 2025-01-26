faa64013afc03e00aaab7c8b07dcf68fd9f85c7d
Jameel Marco <londoholm1@gmail.com>
2025-01-26 16:37:32 +0800

    add sonner and edit profile function with update user session (not a site reload, so other parts of user data are not changed)

--- components/modals/edit-profile-modal.tsx
+++ components/modals/edit-profile-modal.tsx
@@ -8,49 +8,53 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { saveProfileChanges } from "@/lib/api";
 import { User } from "@/lib/types";
-import avatarOne from "@/public/avatars/avatarOne.png";
-import avatarThree from "@/public/avatars/avatarThree.png";
-import avatarTwo from "@/public/avatars/avatarTwo.png";
-import plant from "@/public/bg/plant.jpg";
-import sea from "@/public/bg/sea.jpg";
-import shore from "@/public/bg/shore.jpg";
-import { getSession, signIn, useSession } from "next-auth/react";
+import { avatars, banners } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useState } from "react";
-export default function EditProfileModal() {
-  const { data: session, update } = useSession();
-  const avatars = [avatarOne, avatarTwo, avatarThree];
-  const banners = [plant, shore, sea];
-  const credits = ["Junel Mujar", "Marc Kleen", "Rafael Garcin"];
+import { toast } from "sonner";
 
+export default function EditProfileModal() {
+  const { data: session } = useSession();
+  const user = session?.user as User;
   const [username, setUsername] = useState("");
-  const [avatarSelected, setAvatarSelected] = useState("");
-  const [bannerSelected, setBannerSelected] = useState("");
+  const [avatarSelected, setAvatarSelected] = useState(user?.avatar);
+  const [bannerSelected, setBannerSelected] = useState(user?.banner);
+  const [isLoading, setIsLoading] = useState(false);
+
   async function handleSaveChanges() {
+    setIsLoading(true);
+
     const updatedUser = {
       ...session?.user,
       banner: bannerSelected,
       // username: username,
       avatar: avatarSelected,
     };
+
+    saveProfileChanges(updatedUser as User);
     console.log(session?.user, updatedUser);
 
-    try {
-      // Update the session with new data
-      await update({ user: updatedUser });
-
-      // Optional: Refresh session from backend
-      await signIn("credentials", { redirect: false });
-
-      const t = await getSession();
-
-      console.log("Profile updated successfully:", t, updatedUser, session?.user);
-    } catch (error) {
-      console.error("Failed to update profile:", error);
-    }
+    signIn("credentials", {
+      formData: JSON.stringify(updatedUser),
+      mode: "update",
+      redirect: false,
+    })
+      .then((response) => {
+        if (response?.error) {
+          toast("Changes Not Saved!", {
+            description: response?.error,
+          });
+        } else {
+          toast("Changes saved successfully!");
+        }
+      })
+      .catch((error) => console.error("An unexpected error occurred:", error))
+      .finally(() => setIsLoading(false));
   }
 
   return (
@@ -58,40 +62,40 @@ export default function EditProfileModal() {
       <DialogTrigger asChild>
         <Button variant="default">Edit Profile</Button>
       </DialogTrigger>
-      <DialogContent className="w-[20rem] sm:max-w-[425px]">
+      <DialogContent className="w-[23rem] sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
         </DialogHeader>
         <div className="grid gap-4 py-4 ">
-          <div className="grid grid-cols-3 items-center gap-4">
+          {/* <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="username" className="text-right">
               Username
             </Label>
             <Input
               id="username"
               onChange={(e) => setUsername(e.target.value)}
-              className="col-span-2"
+              className="col-span-3"
               defaultValue={(session?.user as User)?.username}
             />
-          </div>
-          <div className="grid grid-cols-3 items-center gap-4">
+          </div> */}
+          <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="username" className="text-right">
               Avatar
             </Label>
-            <div className="flex w-full text-slate-400 col-span-2 gap-2 items-center">
-              Morty{" "}
+            <div className="flex w-full text-slate-400 col-span-3 gap-2 items-center justify-end">
+              {avatarSelected?.name || "Jacket"}
               <div className="flex">
                 {avatars.map((a, index) => {
                   return (
                     <Image
                       key={index}
-                      src={a.src}
-                      onClick={() => setAvatarSelected(a.src)}
+                      src={a.img}
+                      onClick={() => setAvatarSelected(a)}
                       alt={`avatar ${index + 1}`}
                       width={300}
                       height={300}
                       className={`${
-                        avatarSelected == a.src && "border-2 border-white"
+                        avatarSelected?.name == a.name && "border-2 border-white"
                       } object-cover h-10 w-10`}
                     />
                   );
@@ -99,24 +103,24 @@ export default function EditProfileModal() {
               </div>
             </div>
           </div>
-          <div className="grid grid-cols-3 items-center gap-4">
+          <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="username" className="text-right">
               Banner
             </Label>
-            <div className="flex gap-2 text-slate-400 col-span-2 items-center">
-              Moss{" "}
+            <div className="flex gap-2 text-slate-400 col-span-3 items-center justify-end">
+              {bannerSelected?.name || "Moss"}
               <div className="flex">
                 {banners.map((a, index) => {
                   return (
                     <Image
                       key={index}
-                      onClick={() => setBannerSelected(a.src)}
-                      src={a.src}
+                      onClick={() => setBannerSelected(a)}
+                      src={a.img}
                       alt={`banner ${index + 1}`}
                       width={300}
                       height={300}
                       className={`${
-                        bannerSelected == a.src && "border-2 border-white"
+                        bannerSelected?.name === a.name && "border-2 border-white"
                       } object-cover h-10 w-10`}
                     />
                   );
@@ -126,8 +130,15 @@ export default function EditProfileModal() {
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit" onClick={handleSaveChanges} className="w-full">
-            Save changes
+          <Button type="submit" disabled={isLoading} onClick={handleSaveChanges} className="w-full">
+            {isLoading ? (
+              <>
+                <Loader2 className="mr-2 h-4  animate-spin" />
+                Loading
+              </>
+            ) : (
+              "Save changes"
+            )}
           </Button>
           <DialogClose asChild>
             <Button variant="secondary" className="w-full">
